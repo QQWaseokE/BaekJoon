255260aca7bf170c3dcfa1a0b41910c744767f0e
QQWaseokE <127533265+QQWaseokE@users.noreply.github.com>
2023-09-25 20:29:31 +0900

    [Bronze V] Title: 개, Time: 108 ms, Memory: 13116 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    println("|\\_/|")
+    println("|q p|   /}")
+    println("( 0 )\"\"\"\\")
+    println("|\"^\"`    |")
+    println("||_/=\\\\__|")
+}

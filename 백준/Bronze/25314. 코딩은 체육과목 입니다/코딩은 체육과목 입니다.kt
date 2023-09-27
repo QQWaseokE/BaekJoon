1c2b8b0dd908eec92615e457b5b3fa35380a15e2
QQWaseokE <127533265+QQWaseokE@users.noreply.github.com>
2023-09-27 20:35:01 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 120 ms, Memory: 13280 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    var n = nextInt()
+    var a : Int = n/4
+
+    for(i : Int in 1..a){
+        print("long ")
+    }
+    print("int")
+}

7cc1bc0db360a3f0e691872aeceea0dc799e7895
QQWaseokE <127533265+QQWaseokE@users.noreply.github.com>
2023-09-27 21:06:25 +0900

    [Bronze V] Title: A+B - 7, Time: 96 ms, Memory: 12380 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.kt
@@ -0,0 +1,19 @@
+import java.util.*
+import java.io.*;
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    var writer = BufferedWriter(OutputStreamWriter(System.out))
+
+    for(i in 1..readLine().toInt()){
+        StringTokenizer(readLine()).run{
+            val a = nextToken().toInt()
+            val b = nextToken().toInt()
+
+            writer.write("Case #$i: ${a+b}\n")
+        }
+    }
+
+    writer.flush()
+    writer.close()
+    close()
+}

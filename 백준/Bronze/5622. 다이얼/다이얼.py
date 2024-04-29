ebcb47e82f2b25d8c39d0cfcca5db36d3b60ef50
QQWaseokE <127533265+QQWaseokE@users.noreply.github.com>
2024-04-29 12:58:29 +0900

    [Bronze II] Title: 다이얼, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,28 +1,38 @@
-str = list(input())
-n = len(str)
-sum = 0
+def number(a):
 
-for i in range(n):
-    if(str[i] == 'A' or str[i] == 'B' or str[i] == 'C'):
-        str[i] = 3
-    elif(str[i] == 'D' or str[i] == 'E' or str[i] == 'F'):
-        str[i] = 4
-    elif(str[i] == 'G' or str[i] == 'H' or str[i] == 'I'):
-        str[i] = 5
-    elif(str[i] == 'J' or str[i] == 'K' or str[i] == 'L'):
-        str[i] = 6
-    elif(str[i] == 'M' or str[i] == 'N' or str[i] == 'O'):
-        str[i] = 7
-    elif(str[i] == 'P' or str[i] == 'Q' or str[i] == 'R' or str[i] == 'S'):
-        str[i] = 8
-    elif(str[i] == 'T' or str[i] == 'U' or str[i] == 'V'):
-        str[i] = 9
-    elif(str[i] == 'W' or str[i] == 'X' or str[i] == 'Y' or str[i] == 'Z'):
-        str[i] = 10
-    else:
-        str[i] = 2
+    str_list = list(a)
 
-for i in range(n):
-    sum += str[i]
-    
-print(sum)
+    n = len(str_list)
+
+    sum = 0
+
+    for i in range(0, n):
+        if str_list[i] == "A" or str_list[i] == "B" or str_list[i] == "C":
+            str_list[i] = 3
+        elif str_list[i] == "D" or str_list[i] == "E" or str_list[i] == "F":
+            str_list[i] = 4
+        elif str_list[i] == "G" or str_list[i] == "H" or str_list[i] == "I":
+            str_list[i] = 5
+        elif str_list[i] == "J" or str_list[i] == "K" or str_list[i] == "L":
+            str_list[i] = 6
+        elif str_list[i] == "M" or str_list[i] == "N" or str_list[i] == "O":
+            str_list[i] = 7
+        elif (
+            str_list[i] == "P"
+            or str_list[i] == "Q"
+            or str_list[i] == "R"
+            or str_list[i] == "S"
+        ):
+            str_list[i] = 8
+        elif str_list[i] == "T" or str_list[i] == "U" or str_list[i] == "V":
+            str_list[i] = 9
+        elif str_list[i] == "W" or "X" or str_list[i] == "Y" or str_list[i] == "Z":
+            str_list[i] = 10
+
+        sum = sum + str_list[i]
+
+    print(sum)
+
+
+str = input()
+number(str)
